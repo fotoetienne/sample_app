a1255936bb05fc3d8f4da994c5a1542b05f11ee2
stephen <root@lmsnhost>
2011-04-06 21:20:58 -0400

    Added some password stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,37 @@
 #
 
 class User < ActiveRecord::Base
-    attr_accessible :name, :email
-    
-    email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessor :password 
+  attr_accessible :name, :email, :password, :password_confirmation
+   
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-    validates :name,  :presence   => true,
-                      :length     => { :maximum => 50 }
-    validates :email, :presence   => true,
-                      :format     => { :with => email_regex },
-                      :uniqueness => { :case_sensitive => false }
+  validates :name,     :presence   => true,
+                       :length     => { :maximum => 50 }
+  validates :email,    :presence   => true,
+                       :format     => { :with => email_regex },
+                       :uniqueness => { :case_sensitive => false }
+  validates :password, :presence     => true,
+                       :confirmation => true,
+                       :length       => { :within => 6..40 }
+
+  before_save :encrypt_password
+
+  # Return true if the user's password matches the submitted password.
+  def has_password?(submitted_password)
+    # Compare encrypted_password with the encrypted version of
+    # submitted_password.
+    encrypted_password == encrypt(submitted_password)
+  end
+
+  private
+
+    def encrypt_password
+      self.encrypted_password = encrypt(password)
+    end
+
+    def encrypt(string)
+      string # Only a temporary implementation!
+    end
+
 end
